fc442f39c0e70f0d6276af9770408ec9f993592c
weezyiswain <tywain@microsoft.com>
2025-10-03 19:01:48 +0000

    updated vite config file

--- vite.config.ts
+++ vite.config.ts
@@ -1,11 +1,7 @@
-import { defineConfig } from "vite";
-import react from "@vitejs/plugin-react";
+import { defineConfig } from 'vite'
 
-// https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
   build: {
-    outDir: "dist",   // ensures build goes into dist/
-    emptyOutDir: true // cleans dist/ before each build
+    outDir: 'dist'
   }
-});
+})
